2c13ef8bf67255282b293a6df54cf7c8287ba692
hehe <646546098@qq.com>
2017-07-18 08:04:28 +0800

    -完成前19题

---
+++ leetcodeJava/src/Remove_Nth_Node_From_End_of_List_19/ListNode.java
@@ -0,0 +1,12 @@
+package Remove_Nth_Node_From_End_of_List_19;
+
+/**
+ * Created by yanqinghe on 2017/7/16.
+
+ * Definition for singly-linked list.
+ * */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
