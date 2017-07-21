263b60ac1da56a9cf4496813f4b65627ac1c2255
hehe <hehelemon@outlook.com>
2017-07-21 23:17:25 +0800

    完成第27题

---
+++ leetcodeJava/src/Remove_Element_27/Solution.java
@@ -0,0 +1,15 @@
+class Solution{
+    public static void main(String[] args){
+        
+    }
+    public int removeElement(int[] nums, int val) {
+        int result =0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[result]=nums[i];
+            result++;
+            }
+        }
+        return result;
+    }
+}
