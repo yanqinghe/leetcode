57280e4879a8eae21195d352a98648cc50b4f4ea
yanqinghe <hehelemon@outlook.com>
2017-10-04 12:19:15 +0800

    -finish 72th problem

---
+++ leetcodeJava/src/LeetCode72_Edit_Distance/Solution72.java
@@ -0,0 +1,30 @@
+package LeetCode72_Edit_Distance;
+
+public class Solution72 {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m+1][n+1];
+        for(int i=0;i<=n;i++){
+            dp[0][i]= i;
+        }
+        for (int i = 0; i <= m; i++) {
+            dp[i][0]=i;
+        }
+        for(int i =1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
+                    //如果相等
+                    dp[i][j]=dp[i-1][j-1];
+                }else {
+                    //如果不等
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }
+                dp[i][j]=Math.min(dp[i][j],Math.min(dp[i-1][j]+1,dp[i][j-1]+1));
+
+            }
+        }
+        return dp[m][n];
+    }
+}
+
