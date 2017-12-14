41fe1c9c7c693bc20992c98405b2140e1725421b
hehe <hehelemon@outlook.com>
2017-12-14 21:49:38 +0800

    -啊啊啊

---
+++ src/LeetCode213_House_Robber_II/Solution213.java
@@ -0,0 +1,37 @@
+package LeetCode213_House_Robber_II;
+
+public class Solution213 {
+    public static void main(String[] args) {
+        Solution213 solution213 = new Solution213();
+        solution213.rob(new int[]{11,2,3,4,1,25,51,23,5,6,3,41,5,65});
+    }
+    public int rob(int[] nums) {
+        if(nums==null||nums.length<1) return 0;
+        if(nums.length==1) return nums[0];
+        int [] help = new int[4];
+        int [] help3 = new int[4];
+        boolean [] help2 = new boolean[4];
+        help[3]=nums[0];
+        help2[3] =true;
+        help3[3]=0;
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < help.length-1; j++) {
+                help[j]=help[j+1];
+                help2[j]=help2[j+1];
+                help3[j]=help3[j+1];
+            }
+            help3[3]=Math.max(help3[0],help3[1])+nums[i];
+            if(help[0]>help[1]){
+                help[3]=help[0]+nums[i];
+                help2[3]=help2[0];
+            }else if(help[0]<help[1]){
+                help[3]=help[1]+nums[i];
+                help2[3]=help2[1];
+            }else{
+                help[3]=help[1];
+                help2[3]=help2[0]&&help2[1];
+            }
+        }
+        return Math.max(Math.max(help3[2],help3[3]),Math.max(help[2],help2[3]?Math.max(help[3]-nums[0],help[3]-nums[nums.length-1]):help[3]));
+    }
+}
---
+++ src/leetCode214_Shortest_Palindrome/Solution214.java
@@ -0,0 +1,33 @@
+package leetCode214_Shortest_Palindrome;
+
+public class Solution214 {
+    public String shortestPalindrome(String s) {
+        if(s==null||s.length()<1) return s;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = s.length(); i >0; i--) {
+            if(!isP(s.substring(0,i),0,i-1)){
+                stringBuilder.append(s.charAt(i-1));
+            }
+        }
+        return stringBuilder.append(s).toString();
+    }
+    boolean isP(String str,int i ,int j){
+        if(i==j||i==j-1) return true;
+        if(str.charAt(i)==str.charAt(j)){
+            return isP(str,i+1,j-1);
+        }
+        return false;
+    }
+    public String shortestPalindrome2(String s) {
+        String r = new StringBuilder(s).reverse().toString();
+        String t = s + "#" + r;
+        int[] next = new int[t.length()];
+        for (int i = 1; i < t.length(); ++i) {
+            int j = next[i - 1];
+            while (j > 0 && t.charAt(i) != t.charAt(j)) j = next[j - 1];
+            j += (t.charAt(i) == t.charAt(j)) ? 1 : 0;
+            next[i] = j;
+        }
+        return r.substring(0, s.length() - next[t.length() - 1]) + s;
+    }
+}
